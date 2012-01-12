0c32b52b85169603e9afea2861e7e24ee4a43c43
Eric Martin Christiansen <ericmartinchristiansen@gmail.com>
2012-01-12 13:13:00 -0800

    added accuracy benchmark code for LSH, which is an approximate method

--- src/main/scala/benchmark.scala
+++ src/main/scala/benchmark.scala
@@ -10,6 +10,31 @@ import BenchmarkUtil._
 // but I don't know Scala well enough to get it to work. So I gave up and did
 // things this way.
 
+object RunAccuracy extends App {
+  val data_filename = "data/letter-recognition.data"
+  val hash_dimension = 64
+  val num_permutations = 16
+
+  type TestMetric = L2Vector
+
+  def MakeTestMetric = (data: Seq[Double]) => new TestMetric(data: _*)
+  def MakeTestNNFinder = 
+      (data: IndexedSeq[TestMetric]) => new MetricLSHNN(data, hash_dimension, num_permutations)
+
+  val data = { 
+    val data = LoadDoubleData(data_filename).take(10000)
+    random.shuffle(data.toIndexedSeq)
+  }
+
+  val accuracy = Accuracy(      
+       MakeTestNNFinder,
+       MakeTestMetric,
+       data,
+       3)
+
+  println("accuracy: %.8f".format(accuracy))
+}
+
 object RunBenchmark extends App {
   val data_filename = "data/iris.data"
   type TestMetric = L2Vector
@@ -33,3 +58,5 @@ object RunBenchmark extends App {
 
   println("total time, num metric evals: %.8f, %d".format(total_time, num_metric_evals))
 }
+
+
--- src/main/scala/benchmark_util.scala
+++ src/main/scala/benchmark_util.scala
@@ -15,14 +15,53 @@ object BenchmarkUtil {
     }
   }
 
-  def LoadDoubleData(path: String): List[List[Double]] = { 
+  def LoadDoubleData(path: String): List[IndexedSeq[Double]] = { 
     // Ignore empty lines.
     val lines = Resource.fromFile(path).lines().filter(_.size > 0).toList
 
     val Pattern = """[0-9\.]+""".r
-    for (line <- lines) yield {
-      for (number <- Pattern.findAllIn(line).toList) yield number.toDouble
+    val data = for (line <- lines) yield {
+      val list = for (number <- Pattern.findAllIn(line)) yield number.toDouble
+      list.toIndexedSeq
     }
+
+    // All data must have the same dimension.
+    for (d <- data) assert(data(0).size == d.size)
+    
+    data
+  }
+
+  def AccuracySingle[T <: Metric[T]](
+      make_finder: (IndexedSeq[T]) => NNFinder[T],
+      make_metric: (Seq[Double]) => T,
+      test_data: IndexedSeq[Seq[Double]],
+      train_data: IndexedSeq[Seq[Double]]): Double = {
+    val test: IndexedSeq[T] = test_data.map(make_metric)
+    val train: IndexedSeq[T] = train_data.map(make_metric)
+    
+    println("training")
+    val finder = make_finder(train)
+    println("testing")
+    val brute = new BruteNN(train)
+
+    val scores = for (t <- test) yield { 
+      val guess = train(finder.FindNearest(t)._1).Distance(t)
+      val truth = train(brute.FindNearest(t)._1).Distance(t)
+      if (guess == truth) 1.0 else 0.0
+    }
+    
+    scores.sum / scores.size.toDouble
+  }
+
+  def Accuracy[T <: Metric[T]](
+      make_finder: (IndexedSeq[T]) => NNFinder[T],
+      make_metric: (Seq[Double]) => T,
+      data: IndexedSeq[Seq[Double]],
+      fold_depth: Int): Double = { 
+    val folds = BreakIntoFolds(data, fold_depth)
+
+    val scores = folds.map(f => AccuracySingle(make_finder, make_metric, f._1, f._2))
+    scores.sum / scores.size.toDouble
   }
 
   def BenchmarkSingle[T <: Metric[T]](
